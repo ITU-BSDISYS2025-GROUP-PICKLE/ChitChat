42f8795241ce8d115fa1e5c2791449a42e16361e
oshe <oshe@itu.dk>
2025-10-29 15:52:26 +0100

    Server main function
    Co-Authored-By: Casper <225747490+shtaroo@users.noreply.github.com>

--- server/server.go
+++ server/server.go
@@ -2,12 +2,16 @@ package main
 
 import (
 	pb "ChitChat/proto"
+	"flag"
+	"fmt"
+	"log"
+	"net"
+	"os"
 	"sync"
-)
+	"time"
 
-func main() {
-	// Empty func is empty...
-}
+	"google.golang.org/grpc"
+)
 
 // Server constructor
 type chitChatServer struct {
@@ -15,4 +19,43 @@ type chitChatServer struct {
 
 	mu      sync.Mutex  // Protects the server's latest message from being overridden twice at once
 	lastMsg *pb.Message // The server's latest message
+}
+
+func main() {
+	flag.Parse()
+
+	// Create listener
+	lis, err := net.Listen("tcp", "localhost:50051")
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+
+	// Create server
+	grpcServer := grpc.NewServer()
+	chitChatServer := &chitChatServer{lastMsg: &pb.Message{ClientName: "", Message: "", Time: 0}}
+	pb.RegisterChitChatServer(grpcServer, chitChatServer)
+
+	// Create log file
+	year := fmt.Sprint(time.Now().Year())
+	month := fmt.Sprint(int(time.Now().Month()))
+	day := fmt.Sprint(time.Now().Day())
+	hour := fmt.Sprint(time.Now().Hour())
+	minute := fmt.Sprint(time.Now().Minute())
+	second := fmt.Sprint(time.Now().Second())
+
+	fileName := "server/logs/log-" + year + month + day + "-" + hour + minute + second + ".txt"
+
+	logFile, err := os.OpenFile(fileName, os.O_CREATE|os.O_APPEND|os.O_RDWR, 0644)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer logFile.Close()
+
+	log.SetOutput(logFile)
+
+	// Start server
+	fmt.Println("Server listening on: localhost:50051")
+	log.Println("Server started. Listening on: localhost:50051")
+	defer log.Println("Server shutting down.")
+	grpcServer.Serve(lis)
 }
