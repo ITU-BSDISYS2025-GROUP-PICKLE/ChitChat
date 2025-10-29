423e417e11ef4ffdce8e4463add99fa71991c6de
oshe <oshe@itu.dk>
2025-10-29 15:42:40 +0100

    Server constructor
    Co-Authored-By: Casper <225747490+shtaroo@users.noreply.github.com>

--- server/server.go
+++ server/server.go
@@ -1,5 +1,18 @@
 package main
 
+import (
+	pb "ChitChat/proto"
+	"sync"
+)
+
 func main() {
 	// Empty func is empty...
+}
+
+// Server constructor
+type chitChatServer struct {
+	pb.UnimplementedChitChatServer
+
+	mu      sync.Mutex  // Protects the server's latest message from being overridden twice at once
+	lastMsg *pb.Message // The server's latest message
 }
